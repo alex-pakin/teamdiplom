f62729bf5ee7a09310bbd378681146b72e75a535
alex-pakin <aleksei_pakin@mail.ru>
2022-06-29 13:44:17 +0300

    added new tests for GameStoreTest class

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -14,5 +14,127 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    // Баг - в каталоге видна только первая добавленая игра, остальные не видит//
+    @Test
+    void shouldAddManyGames() {
+        GameStore store = new GameStore();
+
+        Game game1 = store.publishGame("Игра 1", "Спорт");
+        Game game2 = store.publishGame("Игра 2", "Файтинг");
+        Game game3 = store.publishGame("Игра 3", "Гонки");
+
+        assertTrue(store.containsGame(game3));
+    }
+
+    // Баг - игра в каталоге, при этом считает,что ее нет//
+    @Test
+    void shouldNotContainGame() {
+        GameStore store1 = new GameStore();
+        GameStore store2 = new GameStore();
+
+        Game game = store1.publishGame("Игра 1", "Аркады");
+        assertFalse(store1.containsGame(game));
+
+    }
+
+    // Баг - Не отображается количество времени 1 игрока в игре//
+    @Test
+    void shouldAddPlayTimeOfOnePlayer() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+        store.addPlayTime("Леша", 33);
+
+        int actual = store.getSumPlayedTime();
+        int expected = 33;
+
+        assertEquals(expected,actual);
+    }
+
+    // Баг - Не суммируется время игры 1 игрока в игре//
+    @Test
+    void shouldAddPlayTimeOfOnePlayerInTimePeriod() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+        store.addPlayTime("Леша", 3);
+        store.addPlayTime("Леша", 2);
+        store.addPlayTime("Леша", 9);
+        store.addPlayTime("Леша", 10);
+        store.addPlayTime("Леша", 1);
+
+        int actual = store.getSumPlayedTime();
+        int expected = 25;
+
+        assertEquals(expected,actual);
+    }
+
+    // Баг - Не отображается количество времени всех игроков в игре//
+    @Test
+    void shouldAddPlayTimeOfManyPlayers() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+        store.addPlayTime("Леша", 18);
+        store.addPlayTime("Саша", 5);
+        store.addPlayTime("Аля", 44);
+        store.addPlayTime("Надя", 10);
+        store.addPlayTime("Света", 1);
+
+        int actual = store.getSumPlayedTime();
+        int expected = 78;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldGetMostPlayedAmongPlayers() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+
+        store.addPlayTime("Леша", 18);
+        store.addPlayTime("Саша", 5);
+        store.addPlayTime("Аля", 44);
+        store.addPlayTime("Надя", 10);
+        store.addPlayTime("Света", 1);
+
+        String actual = store.getMostPlayer();
+        String expected = "Аля";
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldGetMostPlayedIfNobodyPlayed() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+
+        String actual = store.getMostPlayer();
+        String expected = null;
+        assertEquals(null,actual);
+    }
+
+    @Test
+    void shouldGetMostPlayedIfPlayersHaveZeroCount() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+
+        store.addPlayTime("Леша", 0);
+        store.addPlayTime("Саша", 0);
+        store.addPlayTime("Аля", 0);
+
+        String actual = store.getMostPlayer();
+        String expected = null;
+        assertEquals(null,actual);
+    }
+
+    @Test
+    void shouldGetMostPlayedIfPlayersHaveEqualCount() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Игра 1", "Файтинг");
+
+        store.addPlayTime("Леша", 2);
+        store.addPlayTime("Саша", 2);
+        store.addPlayTime("Аля", 2);
+
+        String actual = store.getMostPlayer();
+        String expected = "Аля";
+        assertEquals(expected,actual);
+    }
+
 }
